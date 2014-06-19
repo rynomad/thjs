6cd4d75bd138e44026446c7bda59e01cb6c26ee7
Ryan Bennett <nomad.ry@gmail.com>
2014-06-19 14:07:03 -0600

    begin refactoring whois. Channel is bulked but needs scanning

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ var debug    = require("./lib/debug.js").debug
   , defaults = require("./lib/defaultConfig.js")
   , Switch = require("./lib/Switch.js")
   , info     = function(){}
-  , pathShareOrder = ["bluetooth","webrtc","ipv6","ipv4","http"]
   , exports  = {
       info     : function(cb){ info = cb; }
     , debug    : debug
     , defaults : defaults
-    , switch   : thSwitch
+    , switch   : Switch
   }
